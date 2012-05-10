2551f514719b18afc793870684c3759775dedbec
Thomas Upton <thomasupton@gmail.com>
2012-05-09 19:44:20 -0500

    More robust build commands.
    Use absolute paths of commands so aliases that, for example, add color or line numbers to grep don't
    interfere with the output.
    Use `tr -d` instead of generating a warning about an empty replacement string.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd jquery-syntaxhighlighter
 outfile="../pkg/github.com.js"
 echo -n var _INLINE_CSS = \" > $outfile
 cat prettify/prettify.css styles/style.css styles/theme-balupton.css \
-	| grep -v '^/[*]' | tr '\n' '' >>  $outfile
+	| /usr/bin/grep -v '^/[*]' | /usr/bin/tr -d '\n' >>  $outfile
 echo \"\; >> $outfile
 cat ../src/header.js \
 	./scripts/resources/core.console.js \
