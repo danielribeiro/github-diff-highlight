b43f2e1ae4cac865a1b638f483e5076340ffa384
Thomas Upton <thomasupton@gmail.com>
2012-05-09 20:20:18 -0500

    Better identification of commit and pull request pages.

--- src/header.js
+++ src/header.js
@@ -1,5 +1,5 @@
 $(function() {
 	var pathname = window.location.pathname;
-	if (pathname.indexOf('commit') === -1 && pathname.indexOf('pull') === -1) {
+	if (pathname.indexOf('/commit/') === -1 && pathname.indexOf('/pull/') === -1) {
 		return;
 	}
