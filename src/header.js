5d32ae6c68a49f54433d01ff7532f67b63f8361d
Thomas Upton <thomasupton@gmail.com>
2012-05-10 11:22:42 -0500

    JQuery.inArray actually returns the index of the item.
    In this case, the item at index 0 was not getting matched.

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 $(function() {
 	var valid_actions = ['commit', 'pull', 'pulls'],
 	    action = window.location.pathname.split("/")[3];
-	if (!$.inArray(action, valid_actions)) {
+	if ($.inArray(action, valid_actions) === -1) {
 		return;
 	}
