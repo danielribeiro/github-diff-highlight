e35ccaa997e64c6ba5f38c43c6e7dca67aaf4c84
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-13 23:52:47 -0700

    build works again

--- src/footer.js
+++ src/footer.js
@@ -1,12 +1,6 @@
-	setTimeout(function(){$(".diff-view pre").addClass("highlight");
-    $(".diff-line").each(function() {
-        var text = $.trim($(this).text());
-        $(this).html("<pre></pre>");
-        $(this).find("pre").text(text);
-    }) 
-	$.SyntaxHighlighter.init({lineNumbers: false});
-
-
-	$("td.gd.diff-line pre").css({backgroundColor: "#FDD"});
-	$("td.gi.diff-line pre").css({backgroundColor: "#DFD"});}, 900);
+    $(".diff-line").each(function() {var text = $.trim($(this).text()); $(this).html("<pre></pre>"); $(this).find("pre").text(text); }) 
+    $(".diff-line pre").addClass("highlight");
+    $("td.gd.diff-line pre").css({backgroundColor: "#FDD"});
+    $("td.gi.diff-line pre").css({backgroundColor: "#DFD"});
+	setTimeout(function(){ $.SyntaxHighlighter.init({lineNumbers: false}); }, 900);
 });
