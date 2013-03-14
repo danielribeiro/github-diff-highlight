f3b7b4ced30c9d296ca1d4898d85a92fb2a93ba5
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-13 23:40:50 -0700

    making it work with new github layout

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,12 @@
 	setTimeout(function(){$(".diff-view pre").addClass("highlight");
+    $(".diff-line").each(function() {
+        var text = $.trim($(this).text());
+        $(this).html("<pre></pre>");
+        $(this).find("pre").text(text);
+    }) 
 	$.SyntaxHighlighter.init({lineNumbers: false});
 
+
 	$("td.gd.diff-line pre").css({backgroundColor: "#FDD"});
-	$("td.gi.diff-line pre").css({backgroundColor: "#DFD"});}, 200);
+	$("td.gi.diff-line pre").css({backgroundColor: "#DFD"});}, 900);
 });
