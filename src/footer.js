3736c92fa575b6c4e9e04a1cb0ff4592de05baa1
Daniel Ribeiro <danielrb@pagerduty.com>
2013-04-15 22:05:59 -0700

    not higlight diffs in discussion, as it uses outdated css

--- src/footer.js
+++ src/footer.js
@@ -1,4 +1,6 @@
-    $(".diff-line-code").each(function() {
+    $("pre.diff-line-pre").not('#discussion_bucket pre.diff-line-pre')
+    var sel = function(s) { return $(s).not('#discussion_bucket ' + s); }
+    sel(".diff-line-code").each(function() {
         var t = $(this);
         var text = $.trim(t.text());
         t.contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).remove()
@@ -6,8 +8,8 @@
         t.append("<pre></pre>");
         t.find("pre").text(text);
     }) 
-    $(".diff-line-code pre").addClass("highlight");
-    $(".file-diff-line.gd .diff-line-code pre").css({backgroundColor: "#FDD"});
-    $(".file-diff-line.gi .diff-line-code pre").css({backgroundColor: "#DFD"});
+    sel(".diff-line-code pre").addClass("highlight");
+    sel(".file-diff-line.gd .diff-line-code pre").css({backgroundColor: "#FDD"});
+    sel(".file-diff-line.gi .diff-line-code pre").css({backgroundColor: "#DFD"});
 	setTimeout(function(){ $.SyntaxHighlighter.init({lineNumbers: false}); }, 200);
 });
